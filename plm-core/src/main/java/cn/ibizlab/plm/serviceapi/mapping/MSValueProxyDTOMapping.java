20256602beab2c8e6f21e292b8f96d1548d8ad47
sq3536 <sq3536@qq.com>
2024-05-10 18:06:50 +0800

    update: generator适配模型版本升级V1.0.20240506

---
+++ plm-core/src/main/java/cn/ibizlab/plm/serviceapi/mapping/MSValueProxyDTOMapping.java
@@ -0,0 +1,22 @@
+/**
+ * Generate code from /{{projectName}}-core/src/main/java/{{packageName}}/{{apis}}/mapping/{{apiDtos@DEFAULT}}Mapping.java.hbs
+ */
+package cn.ibizlab.plm.serviceapi.mapping;
+
+import org.mapstruct.*;
+import cn.ibizlab.plm.core.extension.domain.MSValueProxy;
+import cn.ibizlab.plm.serviceapi.dto.MSValueProxyDTO;
+import cn.ibizlab.util.domain.MappingBase;
+
+/**
+ * 主状态值代理转换[MSValueProxyDTO]
+ *
+ * @author generator
+ */
+@Mapper(componentModel = "spring", uses = {}, implementationName = "ServiceAPIMSValueProxyDTOMapping",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface MSValueProxyDTOMapping extends MappingBase<MSValueProxyDTO, MSValueProxy> {
+
+
+}
